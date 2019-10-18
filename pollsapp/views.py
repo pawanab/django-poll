b62ee62ec39055e10166938d2d5bdb7d8fc93693
Pawan <pawanab@gmail.com>
2019-10-18 18:01:40 +0530

    generic view implment

--- pollsapp/views.py
+++ pollsapp/views.py
@@ -3,27 +3,32 @@ from .models import Question, Choice
 from django.template import loader
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
+from django.views import generic
 
 # Create your views here.
 
 
-def index(request):
-    latest_question = Question.objects.order_by('-publish_date')[:5]
+class IndexView(generic.ListView):
+    # default template <app name>/<model name>_list.html
+    template_name = 'pollsapp/index.html'
+    # default context object name <model_name>_list ie question_list
+    context_object_name = 'latest_questions'
 
     # template = loader.get_template('pollsapp/index.html')
-    context = {
-        'latest_questions': latest_question
-    }
     # return HttpResponse(template.render(context, request))
-    return render(request, 'pollsapp/index.html', context)
+    def get_queryset(self):
+        return Question.objects.order_by('-publish_date')[:5]
 
 
-def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404('Question does not exists.')
-    return render(request, 'pollsapp/detail.html', {'question': question})
+class DetailView(generic.DetailView):
+    model = Question
+    # default <app name>/<model name>_detail.html
+    template_name = 'pollsapp/detail.html'
+
+
+class ResultView(generic.DetailView):
+    model = Question
+    template_name = 'pollsapp/result.html'
 
 
 def vote(request, question_id):
@@ -43,8 +48,3 @@ def vote(request, question_id):
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
         return HttpResponseRedirect(reverse('polls:result', args=(question.id,)))
-
-
-def result(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'pollsapp/result.html', {'question': question})
