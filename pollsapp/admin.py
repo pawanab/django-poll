6de448087a920fcd5c8049b34c59ad96d36b1726
Pawan <pawanab@gmail.com>
2019-10-21 14:17:26 +0530

    static configured and admin templte override

--- pollsapp/admin.py
+++ pollsapp/admin.py
@@ -3,5 +3,26 @@ from .models import Question, Choice
 
 # Register your models here.
 
-admin.site.register(Question)
-admin.site.register(Choice)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['publish_date'],
+                              'classes':['collapse']})
+    ]
+    inlines = [ChoiceInline]
+
+    # admin list page configuration i.e. which column to display
+    list_display = ('question_text', 'publish_date', 'was_published_recently')
+    # admin page list view page filter optoin (right side widget)
+    list_filter = ['publish_date']
+    # to display search box top of change list
+    search_fields = ['question_text']
+
+
+admin.site.register(Question, QuestionAdmin)
